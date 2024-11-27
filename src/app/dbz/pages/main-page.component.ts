9103e600ace4882bdc59d3258d1a3dadc433d005
Ruben Hernandez <rubenhectorh7@gmail.com>
2024-11-27 16:11:23 -0300

    seccion 5 - clase63

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent {
 
-
-
+  public character: Character[] =
+  [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500
+    },
+    {
+      name: 'Vegeta',
+      power: 7500
+    }
+  ];
 }
